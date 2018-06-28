67834d902e3405a6ecf44cfe2638325ca295ab5b
zhycn <qzhservice@gmail.com>
2018-06-28 22:14:08 +0800

    remove logback.xml

--- scripts/build.bat
+++ scripts/build.bat
@@ -2,19 +2,19 @@
 
 rem apollo config db info
 set apollo_config_db_url="jdbc:mysql://localhost:3306/ApolloConfigDB?characterEncoding=utf8"
-set apollo_config_db_username="root"
-set apollo_config_db_password=""
+set apollo_config_db_username="apollo"
+set apollo_config_db_password="apollo"
 
 rem apollo portal db info
 set apollo_portal_db_url="jdbc:mysql://localhost:3306/ApolloPortalDB?characterEncoding=utf8"
-set apollo_portal_db_username="root"
-set apollo_portal_db_password=""
+set apollo_portal_db_username="apollo"
+set apollo_portal_db_password="apollo"
 
 rem meta server url, different environments should have different meta server addresses
 set dev_meta="http://localhost:8080"
-set fat_meta="http://someIp:8080"
-set uat_meta="http://anotherIp:8080"
-set pro_meta="http://yetAnotherIp:8080"
+set fat_meta="http://localhost:8080"
+set uat_meta="http://localhost:8080"
+set pro_meta="http://localhost:8080"
 
 set META_SERVERS_OPTS=-Ddev_meta=%dev_meta% -Dfat_meta=%fat_meta% -Duat_meta=%uat_meta% -Dpro_meta=%pro_meta%
 
