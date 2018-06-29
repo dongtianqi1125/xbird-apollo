37215b64a41324d5dc71dfc39b02d0d142f1dd41
zhycn <qzhservice@gmail.com>
2018-06-29 10:18:42 +0800

    Deleted ctrip's configs.

--- apollo-biz/src/main/java/com/ctrip/framework/apollo/biz/auth/WebSecurityConfig.java
+++ apollo-biz/src/main/java/com/ctrip/framework/apollo/biz/auth/WebSecurityConfig.java
@@ -1,16 +1,15 @@
 package com.ctrip.framework.apollo.biz.auth;
 
-import com.ctrip.framework.apollo.common.condition.ConditionalOnMissingProfile;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-@ConditionalOnMissingProfile("auth")
+@Profile("noauth")
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
