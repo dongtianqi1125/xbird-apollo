872210b6f0318d2c73b40da70c4d878b261e87d9
zhycn <qzhservice@gmail.com>
2018-06-29 14:56:45 +0800

    Enabled commit.

---
+++ scripts/sql/initdb.sql
@@ -0,0 +1,22 @@
+## ApolloConfigDB
+INSERT INTO apolloconfigdb.`ServerConfig` (`Key`, `Cluster`, `Value`, `Comment`)
+VALUES
+    ('eureka.service.url', 'default', 'http://localhost:8080/eureka/', 'Eureka服务Url，多个service以英文逗号分隔'),
+    ('namespace.lock.switch', 'default', 'false', '一次发布只能有一个人修改开关'),
+    ('item.key.length.limit', 'default', '128', 'item key 最大长度限制'),
+    ('item.value.length.limit', 'default', '20000', 'item value最大长度限制'),
+    ('config-service.cache.enabled', 'default', 'false', 'ConfigService是否开启缓存，开启后能提高性能，但是会增大内存消耗！');
+
+## ApolloPortalDB
+INSERT INTO apolloportaldb.`ServerConfig` (`Key`, `Value`, `Comment`)
+VALUES
+    ('apollo.portal.envs', 'dev', '可支持的环境列表'),
+    ('organizations', '[{\"orgId\":\"TEST1\",\"orgName\":\"样例部门1\"},{\"orgId\":\"TEST2\",\"orgName\":\"样例部门2\"}]', '部门列表'),
+    ('superAdmin', 'apollo', 'Portal超级管理员'),
+    ('api.readTimeout', '10000', 'http接口read timeout'),
+    ('consumer.token.salt', 'someSalt', 'consumer token salt'),
+    ('admin.createPrivateNamespace.switch', 'false', '是否允许项目管理员创建私有namespace');
+INSERT INTO apolloportaldb.`Users` (`Username`, `Password`, `Email`, `Enabled`)
+VALUES
+	('apollo', '$2a$10$7r20uS.BQ9uBpf3Baj3uQOZvMVvB1RN3PYoKE94gtz2.WAOuiiwXS', 'apollo@acme.com', 1);
+INSERT INTO apolloportaldb.`Authorities` (`Username`, `Authority`) VALUES ('apollo', 'ROLE_user');
