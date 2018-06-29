121c6f16e9443c0bbc46a874a776a4ecfcdda793
zhycn <qzhservice@gmail.com>
2018-06-29 15:50:47 +0800

    removed Docker & Assembly

--- apollo-adminservice/src/main/java/com/ctrip/framework/apollo/adminservice/AdminServiceApplication.java
+++ apollo-adminservice/src/main/java/com/ctrip/framework/apollo/adminservice/AdminServiceApplication.java
@@ -1,8 +1,5 @@
 package com.ctrip.framework.apollo.adminservice;
 
-import com.ctrip.framework.apollo.biz.ApolloBizConfig;
-import com.ctrip.framework.apollo.common.ApolloCommonConfig;
-
 import org.springframework.boot.actuate.system.ApplicationPidFileWriter;
 import org.springframework.boot.actuate.system.EmbeddedServerPortFileWriter;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
@@ -12,12 +9,13 @@ import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import com.ctrip.framework.apollo.biz.ApolloBizConfig;
+import com.ctrip.framework.apollo.common.ApolloCommonConfig;
 @EnableAspectJAutoProxy
 @EnableEurekaClient
 @Configuration
-@PropertySource(value = {"classpath:adminservice.properties"})
 @EnableAutoConfiguration
 @EnableTransactionManagement
 @ComponentScan(basePackageClasses = {ApolloCommonConfig.class,
--- apollo-configservice/src/main/java/com/ctrip/framework/apollo/configservice/ConfigServiceApplication.java
+++ apollo-configservice/src/main/java/com/ctrip/framework/apollo/configservice/ConfigServiceApplication.java
@@ -1,9 +1,5 @@
 package com.ctrip.framework.apollo.configservice;
 
-import com.ctrip.framework.apollo.biz.ApolloBizConfig;
-import com.ctrip.framework.apollo.common.ApolloCommonConfig;
-import com.ctrip.framework.apollo.metaservice.ApolloMetaServiceConfig;
-
 import org.springframework.boot.actuate.system.ApplicationPidFileWriter;
 import org.springframework.boot.actuate.system.EmbeddedServerPortFileWriter;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
@@ -13,8 +9,11 @@ import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import com.ctrip.framework.apollo.biz.ApolloBizConfig;
+import com.ctrip.framework.apollo.common.ApolloCommonConfig;
+import com.ctrip.framework.apollo.metaservice.ApolloMetaServiceConfig;
 
 /**
  * Spring boot application entry point
@@ -27,7 +26,6 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableAutoConfiguration // (exclude = EurekaClientConfigBean.class)
 @Configuration
 @EnableTransactionManagement
-@PropertySource(value = {"classpath:configservice.properties"})
 @ComponentScan(basePackageClasses = {ApolloCommonConfig.class,
     ApolloBizConfig.class,
     ConfigServiceApplication.class,
